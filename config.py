ede5bb7395859980435ce66374f6f509c457b1fa
Iralim <irik.alimbaev@gmail.com>
2026-01-14 12:34:20 +0500

    Added mail configs

--- config.py
+++ config.py
@@ -1,23 +1,25 @@
 import os
-from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR}/projects.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SAMESITE = "Lax"
 
-
-    MAIL_SERVER = 'mail.iralim.com'
-    MAIL_PORT = 993
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'kk@moimail.ru'
-    MAIL_PASSWORD = 'qJ4qE3sS8bqF3pE8'
-    MAIL_DEFAULT_SENDER = 'mail.iralim.com'
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") == "True"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
 
 
   
