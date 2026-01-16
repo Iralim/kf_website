4323390184aba6d4e39b759a66e0bffc1114dab6
Iralim <irik.alimbaev@gmail.com>
2026-01-16 12:04:01 +0500

    correct passenger

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,23 @@
 import sys
 import os
 
-INTERP = os.path.expanduser("/var/www/u3360972/data/flaskenv/bin/python")
+# путь к проекту Flask
+sys.path.insert(0, "/var/www/u3360972/data/kf")
+
+INTERP = "/var/www/u3360972/data/flaskenv/bin/python"
 if sys.executable != INTERP:
-   os.execl(INTERP, INTERP, *sys.argv)
+    os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append(os.getcwd())
+from run import app as application
 
-from run import application
+
+# import sys
+# import os
+#
+# INTERP = os.path.expanduser("/var/www/u3360972/data/flaskenv/bin/python")
+# if sys.executable != INTERP:
+#    os.execl(INTERP, INTERP, *sys.argv)
+#
+# sys.path.append(os.getcwd())
+#
+# from run import application
